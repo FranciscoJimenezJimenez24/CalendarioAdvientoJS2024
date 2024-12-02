db7bb78f939b75ef7a3ee854937a42c93f2c11d0
FranciscoJimenezJimenez24 <franjimenezjimenez24@gmail.com>
2024-12-02 16:19:18 +0100

    dia 2 mejorado

--- dia2.js
+++ dia2.js
@@ -1,10 +1,20 @@
+// function createFrame(names) {
+//     let maxLength = 0;
+//     names.forEach(name => {
+//         if (maxLength < name.length) {
+//             maxLength = name.length;
+//         }
+//     })
+//     let framework = "*".repeat(maxLength + 4) + "\n";
+//     names.forEach(name => {
+//         framework += "* " + name + " ".repeat((maxLength - name.length) + 1) + "*\n";
+//     })
+//     framework += "*".repeat(maxLength + 4);
+//     return framework;
+// }
+
 function createFrame(names) {
-    let maxLength = 0;
-    names.forEach(name => {
-        if (maxLength < name.length) {
-            maxLength = name.length;
-        }
-    })
+    let maxLength = names.reduce((name1, name2) => Math.max(name1.length, name2.length), names[0]);
     let framework = "*".repeat(maxLength + 4) + "\n";
     names.forEach(name => {
         framework += "* " + name + " ".repeat((maxLength - name.length) + 1) + "*\n";
@@ -13,5 +23,16 @@ function createFrame(names) {
     return framework;
 }
 
-createFrame(["midu"]);
+function createFrame(names) {
+    let maxLength = names.reduce((maxLen, name) => { return Math.max(maxLen, name.length);}, 0); 
+    let framework = "*".repeat(maxLength + 4) + "\n";
+    names.forEach(name => {
+        framework += "* " + name + " ".repeat((maxLength - name.length) + 1) + "*\n";
+    })
+    framework += "*".repeat(maxLength + 4);
+    return framework;
+  }
+
+console.log(maxLength(["a","bb","ccc"]));
+
 
