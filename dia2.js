f07146e3339b6860e03e552f755a53ce03d39790
FranciscoJimenezJimenez24 <franjimenezjimenez24@gmail.com>
2024-12-02 16:19:43 +0100

    dia 2 mejorado

--- dia2.js
+++ dia2.js
@@ -1,20 +1,10 @@
-// function createFrame(names) {
-//     let maxLength = 0;
-//     names.forEach(name => {
-//         if (maxLength < name.length) {
-//             maxLength = name.length;
-//         }
-//     })
-//     let framework = "*".repeat(maxLength + 4) + "\n";
-//     names.forEach(name => {
-//         framework += "* " + name + " ".repeat((maxLength - name.length) + 1) + "*\n";
-//     })
-//     framework += "*".repeat(maxLength + 4);
-//     return framework;
-// }
-
 function createFrame(names) {
-    let maxLength = names.reduce((name1, name2) => Math.max(name1.length, name2.length), names[0]);
+    let maxLength = 0;
+    names.forEach(name => {
+        if (maxLength < name.length) {
+            maxLength = name.length;
+        }
+    })
     let framework = "*".repeat(maxLength + 4) + "\n";
     names.forEach(name => {
         framework += "* " + name + " ".repeat((maxLength - name.length) + 1) + "*\n";
