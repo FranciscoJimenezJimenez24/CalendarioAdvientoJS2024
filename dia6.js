be954e6b6a2f0fea0ed3ef68a37e4103144d2d01
FranciscoJimenezJimenez24 <146998590+FranciscoJimenezJimenez24@users.noreply.github.com>
2024-12-06 16:43:31 +0100

    dia 6 mejorado

--- dia6.js
+++ dia6.js
@@ -1,33 +1,17 @@
 function inBox(box) {
-    let out = false;
-    if (box.some(element => element.includes("*"))){
-        let i = 0;
-        while (i < box.length && !out) {
-            let j = 0;
-            if (i == 0 || i == box.length - 1) {
-                if (box[i].includes("*")) {
-                    out = true;
-                }
-            }else{
-                while (j < box[i].length && !out) {
-                    if (j == 0 || j == box[i].length - 1) {
-                        if (box[i][j] == "*") {
-                            out = true;
-                        }
-                    }
-                    j++;
+    let perfect = true;
+    if (box.some(element => element.includes("*") && (!box[0].includes("*") && !box[box.length - 1].includes("*")))) {
+        box.map(
+            (element) => {
+                if (element[0] == "*" || element[element.length - 1] == "*") {
+                    perfect = false;
                 }
             }
-            i++;
-        }
-        if (out) {
-            return false;
-        } else {
-            return true;
-        }
+        )
     } else {
-        return false;
+        perfect = false;
     }
+    return perfect;
 }
 
 console.log(inBox([
