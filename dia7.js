8233aab9b125f0d7db9356e6bc0b89cb46e13ad1
FranciscoJimenezJimenez24 <146998590+FranciscoJimenezJimenez24@users.noreply.github.com>
2024-12-08 16:19:38 +0100

    Dejo este guardado

--- dia7.js
+++ dia7.js
@@ -10,15 +10,33 @@ function fixPackages(packages) {
             arrayU.push(i);
         }
     }
-    for (let i = 0; i < contador; i++) {
-        let string = "";
-        for (let j = arrayC[(contador - 1) - i]+1;j<=arrayU[i]-1;j++){
-            string += packages[j];
+    let maxLength = arrayC.reduce((maxLen, index) => { return Math.max(maxLen, index); }, 0);
+    if ((arrayU.every(element => element > maxLength)) == false) {
+        let firstElementC = arrayC.shift();
+        let lastElementU = arrayU.pop();
+        for (let i = 0; i < (contador - 1); i++) {
+            let string = "";
+            for (let j = arrayC[(contador - 1) - i] + 1; j <= arrayU[i] - 1; j++) {
+                string += packages[j];
+            }
+            packages = packages.replace(string, string.split("").reverse().join(""));
         }
-        packages = packages.replace(string,string.split("").reverse().join(""));
+        let string = "";
+        for (let i = firstElementC; i < lastElementU; i++) {
+            string += packages[i];
+        }
+        packages = packages.replace(string, string.split("").reverse().join(""));
+    } else {
+        for (let i = 0; i < contador; i++) {
+            let string = "";
+            for (let j = arrayC[(contador - 1) - i] + 1; j <= arrayU[i] - 1; j++) {
+                string += packages[j];
+            }
+            packages = packages.replace(string, string.split("").reverse().join(""));
+        }
     }
-    packages = packages.replaceAll("(","");
-    packages = packages.replaceAll(")","");
+    packages = packages.replaceAll("(", "");
+    packages = packages.replaceAll(")", "");
     return packages;
 }
 console.log(fixPackages('a(cb)de'));
@@ -36,3 +54,5 @@ console.log(fixPackages('abc(def(gh)i)jk'));
 console.log(fixPackages('a(b(c))e'));
 // ➞ "acbe"
 // 1º volteamos "c" → "c", luego "bc" → "cb"
+
+console.log(fixPackages('ab(cd(ef)gh(ij)k)l')); // ➞ "abkjihgfedc"
