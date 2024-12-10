5ecdfc24ff1e71a9385e64d20340c1200cf88a9e
FranciscoJimenezJimenez24 <franjimenezjimenez24@gmail.com>
2024-12-10 16:22:59 +0100

    lo he intentado, pero me sigue dando 2 estrellas

--- dia6.js
+++ dia6.js
@@ -1,42 +1,59 @@
 function inBox(box) {
-    if (box.some(element => element.includes("*")) && (!box[0].includes("*") && !box[box.length - 1].includes("*"))) {
-      return !box.some((element) => element[0] == "*" || element[element.length - 1] == "*")
+  if (box.some(element => element.includes("*")) && (!box[0].includes("*") && !box[box.length - 1].includes("*"))) {
+    return !box.some((element) => element[0] == "*" || element[element.length - 1] == "*")
+  } else {
+    return false;
+  }
+}
+function inBox(box) {
+  let boolean = true;
+  if (box[0].includes("*") || box[box.length - 1].includes("*")) {
+    boolean = false;
+  } else {
+    if (!box.some(element => element.includes("*"))) {
+      boolean = false;
     } else {
-      return false;
+      for (let i = 1; i < box.length - 1; i++) {
+        if (box[i][0] == "*" || box[i][box[i].length - 1] == "*") {
+          boolean = false;
+          i = box.length;
+        }
+      }
     }
   }
-
+  return boolean;
+}
 console.log(inBox([
-    "###",
-    "#*#",
-    "###"
+  "###",
+  "#*#",
+  "###"
 ])); // -> true
 
 
 console.log(inBox([
-    "####",
-    "#* #",
-    "#  #",
-    "####"
+  "####",
+  "#* #",
+  "#  #",
+  "####"
 ])) // ➞ true
 
 console.log(inBox([
-    "*####",
-    "#   #",
-    "#  #*",
-    "####"
+  "*####",
+  "#   #",
+  "#  #*",
+  "####"
 ])) // ➞ false
 
 console.log(inBox([
-    "#####",
-    "#   #",
-    "#   #",
-    "#   #",
-    "#####"
+  "#####",
+  "#   #",
+  "#   #",
+  "#   #",
+  "#####"
 ])); // -> false
 
 console.log(inBox([
-    "#*#",
-    "###",
-    "###"
+  "#*#",
+  "###",
+  "###"
 ])) // ->false
