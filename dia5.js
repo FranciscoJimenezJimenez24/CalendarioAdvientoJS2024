d3091ea786e4b44248ab4dded27e65cad54f2929
FranciscoJimenezJimenez24 <146998590+FranciscoJimenezJimenez24@users.noreply.github.com>
2024-12-11 11:21:04 +0100

    no lo he podido mejorar

--- dia5.js
+++ dia5.js
@@ -1,6 +1,6 @@
 function organizeShoes(shoes) {
     let pairsShoes = [];
-    let controlator = shoes.map(
+    shoes.map(
         (shoei, i) => {
             shoes.map(
                 (shoej, j) => {
@@ -19,10 +19,21 @@ function organizeShoes(shoes) {
 const shoes = [
     { type: 'I', size: 38 },
     { type: 'R', size: 38 },
-    { type: 'I', size: 38 },
-    { type: 'I', size: 38 },
-    { type: 'R', size: 38 }
-]
+    { type: 'R', size: 42 },
+    { type: 'I', size: 41 },
+    { type: 'I', size: 42 }
+];
 
 console.log(organizeShoes(shoes));
 // [38, 42]
+
+const shoes2 = [
+    { type: 'I', size: 38 },
+    { type: 'R', size: 38 },
+    { type: 'I', size: 38 },
+    { type: 'I', size: 38 },
+    { type: 'R', size: 38 }
+];
+
+console.log(organizeShoes(shoes2));
+// [38, 38]
