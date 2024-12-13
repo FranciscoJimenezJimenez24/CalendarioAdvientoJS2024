de46f1cb4fb9127fe7725f2569272ae75b3c5de9
FranciscoJimenezJimenez24 <franjimenezjimenez24@gmail.com>
2024-12-13 18:47:26 +0100

    dia 13 por hacer aun

--- dia13.js
+++ dia13.js
@@ -1,51 +1,44 @@
 function isRobotBack(moves) {
     let x = 0; let y = 0;
     moves = moves.split("");
-    for (let i = 0;i<moves.length;i++){
-        if (moves[i] == "!"){
-            moves[i] = moves[i]+moves[i+1];
-            moves.splice(i+1,1); 
-            switch (moves[i]){
-                case "!L": moves[i]="R"; break;
-                case "!R": moves[i]="L"; break;
-                case "!D": moves[i]="U"; break;
-                case "!U": moves[i]="D"; break;
+    for (let i = 0; i < moves.length; i++) {
+        if (moves[i] == "!") {
+            moves[i] = moves[i] + moves[i + 1];
+            moves.splice(i + 1, 1);
+            switch (moves[i]) {
+                case "!L": moves[i] = "R"; break;
+                case "!R": moves[i] = "L"; break;
+                case "!D": moves[i] = "U"; break;
+                case "!U": moves[i] = "D"; break;
             }
+        } else if (moves[i] == "*") {
+            moves[i] = moves[i + 1];
         }
     }
-    for (let i = moves.length-1;i>=0;i--){
-        if (moves[i] == "*"){
-            moves[i] = moves[i]+moves[i+1];
-            moves.splice(i+1,1); 
-        }else if (moves[i] == "?"){
-            if (moves[i-1] == moves[i+1]){
-                moves[i] = moves[i+1];
-                moves.splice(i-1,1);
-                moves.splice(i,1); 
-            }else{
-                moves.splice(i+1,1); 
-                moves.splice(i,1)
-                moves.splice(i-1,1);
+    for (let i = 0; i < moves.length; i++) {
+        if (moves[i] == "?") {
+            if (moves[i - 1] == moves[i + 1]) {
+                moves[i] = moves[i + 1];
+                moves.splice(i, 2);
+            } else {
+                if (i - 1 == -1) {
+                    moves.splice(i, 2);
+                } else {
+                    moves.splice(i - 1, 3);
+                }
+                i -= 2;
             }
         }
     }
     for (let i = 0; i < moves.length; i++) {
         switch (moves[i]) {
-            case "L": x--;break;
-            case "R": x++;break;
-            case "D": y--;break;
-            case "U": y++;break;
-            case "*L": x -= 2; break;
-            case "*R": x += 2; break;
-            case "*D": y -= 2; break;
-            case "*U": y += 2; break;
+            case "L": x--; break;
+            case "R": x++; break;
+            case "D": y--; break;
+            case "U": y++; break;
         }
     }
-    if (x == 0 && y == 0){
-        return true;
-    }else{
-        return [x,y];
-    }
+    return x == 0 && y == 0 ? true : [x, y];
 }
 
 console.log(isRobotBack('R'))// [1, 0]
@@ -62,6 +55,9 @@ console.log(isRobotBack('R?L'))  // true
 console.log(isRobotBack('U?U')) // [0,1]
 console.log(isRobotBack('*U?U')) // [0,2]
 console.log(isRobotBack('U?D?U')) // true
+console.log(isRobotBack('U?D?UD')) // [0,-1]
+console.log(isRobotBack('U?D?UDU')) // true
+console.log(isRobotBack('DU?D?UDU')) // true
 
 // Ejemplos paso a paso:
 console.log(isRobotBack('R!U?U')) // [1,0]
@@ -74,3 +70,5 @@ console.log(isRobotBack('UU!U?D')) // [0,1]
 // 'U'  -> se mueve arriba
 // '!U' -> se invierte y se convierte en 'D'
 // '?D' -> no se mueve, ya que ya se hizo el movimiento 'D'
+
+console.log(isRobotBack('U!D?R')) // [0,2]
