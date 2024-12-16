ca30ee649177723b6acc828bf7e0e98e2ce1e79c
FranciscoJimenezJimenez24 <franjimenezjimenez24@gmail.com>
2024-12-16 16:04:29 +0100

    dia 15 aun por hacer

--- dia15.js
+++ dia15.js
@@ -1,39 +1,54 @@
 function drawTable(data) {
-    let maxLength = 0;
+    let maxLengthHead = [0, 0];
+    let maxLengthAtribute = [0, 0];
+    let maxLength = [0, 0];
+    let contador = 0;
     for (const element of data) {
-        for (const atribute in element){
-            if (maxLength < atribute.length){
-                maxLength = atribute.length
+        for (const atribute in element) {
+            if (maxLengthHead[contador] < atribute.length){
+                maxLengthHead[contador] = atribute.length;
             }
-            if (element[atribute].length > maxLength){
-                maxLength = element[atribute].length;
+            if (maxLengthAtribute[contador] < element[atribute].toString().length){
+                maxLengthAtribute[contador] = element[atribute].toString().length;
             }
+            contador++;
         }
+        contador = 0;
     }
-    let table = ("+" + "-".repeat(maxLength+2)).repeat(2) + "+\n";
+    if (maxLengthHead[0] > maxLengthAtribute[0]) {
+        maxLength[0] = maxLengthHead[0];
+    } else {
+        maxLength[0] = maxLengthAtribute[0];
+    }
+    if (maxLengthHead[1] > maxLengthAtribute[1]) {
+        maxLength[1] = maxLengthHead[1];
+    } else {
+        maxLength[1] = maxLengthAtribute[1];
+    }
+    let table = "+" + "-".repeat(maxLength[0] + 2) + "+"+"-".repeat(maxLength[1] + 2) + "+\n";
     for (const element of data) {
-        for (const atribute in element){
-            table += "| "+(atribute.charAt(0).toUpperCase() + atribute.slice(1))+" ".repeat((maxLength-atribute.length)+1);
+        for (const atribute in element) {
+            table += "| " + (atribute.charAt(0).toUpperCase() + atribute.slice(1)) + " ".repeat((maxLength[contador] - atribute.length) + 1);
+            contador++;
         }
+        contador = 0;
         break;
     }
-    table += "|\n" + ("+" + "-".repeat(maxLength+2)).repeat(2) + "+\n";
+    table += "|\n"+  "+" + "-".repeat(maxLength[0] + 2) + "+"+"-".repeat(maxLength[1] + 2) + "+\n";
     for (const element of data) {
-        for (const atribute in element){
-            if (isNaN(element[atribute])){
-                table += "| "+element[atribute]+" ".repeat((maxLength-element[atribute].length)+1)
-            }else{
-                table += "| "+element[atribute]+" ".repeat((maxLength-(element[atribute]).toString.length))
-            }
+        for (const atribute in element) {
+            table += "| " + element[atribute] + " ".repeat((maxLength[contador] - element[atribute].toString().length)+1)
+            contador++;
         }
+        contador = 0;
         table += "|\n"
     }
-    table += ("+" + "-".repeat(maxLength+2)).repeat(2)+"+";
+    table += "+" + "-".repeat(maxLength[0] + 2) + "+"+"-".repeat(maxLength[1] + 2) + "+";
     return table;
 }
 
 console.log(drawTable([
     { name: 'Alice', city: 'London' },
-    // { name: 'Bob', city: 'Paris' },
-    // { name: 'Charlie', city: 'New York' }
-  ]))
+    { name: 'Bob', city: 'Paris' },
+    { name: 'Charlie', city: 'New York' }
+]))
