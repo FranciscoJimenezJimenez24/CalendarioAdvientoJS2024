513ea20b2ca194d7d3af6c2d93adc74b378e56d8
FranciscoJimenezJimenez24 <franjimenezjimenez24@gmail.com>
2024-12-17 16:00:56 +0100

    dia 15 hecho

--- dia15.js
+++ dia15.js
@@ -1,53 +1,48 @@
-function drawTable(data) {
-    let maxLengthHead = [0, 0];
-    let maxLengthAtribute = [0, 0];
-    let maxLength = [0, 0];
-    let contador = 0;
+function drawTable2(data) {
+    const keys = Object.keys(data[0])
+    const maxLength = keys.map(key =>
+        Math.max(key.length, ...data.map(row => String(row[key]).length))
+    );
+    let table = "";
+    for (let i = 0;i<maxLength.length; i++) {
+        table += "+" + "-".repeat(maxLength[i] + 2)
+    }
+    table += "+\n";
+    let accumulator = 0;
     for (const element of data) {
         for (const atribute in element) {
-            if (maxLengthHead[contador] < atribute.length){
-                maxLengthHead[contador] = atribute.length;
-            }
-            if (maxLengthAtribute[contador] < element[atribute].toString().length){
-                maxLengthAtribute[contador] = element[atribute].toString().length;
-            }
-            contador++;
+            table += "| " + (atribute.charAt(0).toUpperCase() + atribute.slice(1)) + " ".repeat((maxLength[accumulator] - atribute.length) + 1);
+            accumulator++;
         }
-        contador = 0;
-    }
-    if (maxLengthHead[0] > maxLengthAtribute[0]) {
-        maxLength[0] = maxLengthHead[0];
-    } else {
-        maxLength[0] = maxLengthAtribute[0];
-    }
-    if (maxLengthHead[1] > maxLengthAtribute[1]) {
-        maxLength[1] = maxLengthHead[1];
-    } else {
-        maxLength[1] = maxLengthAtribute[1];
-    }
-    let table = "+" + "-".repeat(maxLength[0] + 2) + "+"+"-".repeat(maxLength[1] + 2) + "+\n";
-    for (const element of data) {
-        for (const atribute in element) {
-            table += "| " + (atribute.charAt(0).toUpperCase() + atribute.slice(1)) + " ".repeat((maxLength[contador] - atribute.length) + 1);
-            contador++;
-        }
-        contador = 0;
+        table += "|\n"
+        accumulator = 0;
         break;
     }
-    table += "|\n"+  "+" + "-".repeat(maxLength[0] + 2) + "+"+"-".repeat(maxLength[1] + 2) + "+\n";
+    for (let i = 0;i<maxLength.length; i++) {
+        table += "+" + "-".repeat(maxLength[i] + 2) 
+    }
+    table += "+\n";
     for (const element of data) {
         for (const atribute in element) {
-            table += "| " + element[atribute] + " ".repeat((maxLength[contador] - element[atribute].toString().length)+1)
-            contador++;
+            table += "| " + element[atribute] + " ".repeat((maxLength[accumulator] - element[atribute].toString().length) + 1)
+            accumulator++;
         }
-        contador = 0;
+        accumulator = 0;
         table += "|\n"
     }
-    table += "+" + "-".repeat(maxLength[0] + 2) + "+"+"-".repeat(maxLength[1] + 2) + "+";
+    for (let i = 0;i<maxLength.length; i++) {
+        table += "+" + "-".repeat(maxLength[i] + 2)
+    }
+    table += "+";
     return table;
 }
 
-console.log(drawTable([
+// console.log(drawTable([
+//     { name: 'Alice', city: 'London' },
+//     { name: 'Bob', city: 'Paris' },
+//     { name: 'Charlie', city: 'New York' }
+// ]))
+console.log(drawTable2([
     { name: 'Alice', city: 'London' },
     { name: 'Bob', city: 'Paris' },
     { name: 'Charlie', city: 'New York' }
