88f79eb3631a66d2c75b0a8f5bea33c5c62413e7
FranciscoJimenezJimenez24 <146998590+FranciscoJimenezJimenez24@users.noreply.github.com>
2024-12-21 12:49:41 +0100

    dia 21 hecho

---
+++ dia21.js
@@ -0,0 +1,58 @@
+function treeHeight(tree) {
+    if (tree == null) return 0;
+    let left = tree.left != null ? treeHeight(tree.left) : 0;
+    let right = tree.right != null ? treeHeight(tree.right) : 0;
+    return Math.max(left, right) + 1;
+}
+const tree = {
+    value: '🎁',
+    left: {
+        value: '🎄',
+        left: {
+            value: '⭐',
+            left: null,
+            right: null
+        },
+        right: {
+            value: '🎅',
+            left: null,
+            right: null
+        }
+    },
+    right: {
+        value: '❄️',
+        left: null,
+        right: {
+            value: '🦌',
+            left: null,
+            right: null
+        }
+    }
+}
+
+// Representación gráfica del árbol:
+//        🎁
+//       /   \
+//     🎄     ❄️
+//    /  \      \
+//  ⭐   🎅      🦌
+
+// Llamada a la función
+console.log(treeHeight(tree));
+// Devuelve: 3
+
+const unbalancedTree = {
+    value: '🎁',
+    left: {
+        value: '🎄',
+        left: {
+            value: '⭐',
+            left: null,
+            right: null
+        },
+        right: null
+    },
+    right: null
+};
+
+console.log(treeHeight(unbalancedTree)); // Devuelve: 3
